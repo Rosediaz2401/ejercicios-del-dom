cbb83c0b9b48bb8a4b1bb211257ab016195bfd76
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-05-20 17:29:17 -0500

    nuevos ejercicios del dom cuenta regresive/reloj/teclado/tema-oscuro

--- index_dom.js
+++ index_dom.js
@@ -1,6 +1,22 @@
+import scrollTopButton from "./button_scroll.js";
 import hamburgerMenu from "./menu_hamburguesa.js";
+import { digitalClock,alarm } from "./reloj.js";
+import { moveBall,shortcuts } from "./teclado.js";
+//import scrollTopButton from "./button_scroll.js";
+//import countdown from "./cuenta_regresiva.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d=document;
 d.addEventListener("DOMContentLoaded",e=>{
-    hamburgerMenu(".panel-btn",".panel", ",menu a");
+    hamburgerMenu(".panel-btn",".panel", ".menu a");
+    digitalClock('#reloj','#activar-reloj','#desactivar-reloj')
+    alarm('assets/mixkit-digital-clock-digital-alarm-buzzer-992.wav','#activar-alarma','#desactivar-alarma')
+    //countdown("countdown","January 24, 2023 08:23:23","Feliz Cumpleanos Rose")
+    scrollTopButton(".scroll-top-btn")
+    darkTheme(".dark-theme-btn","dark-mode")
 });
+
+d.addEventListener('keydown',e =>{
+    shortcuts(e);
+    moveBall(e,'.ball','.stage');
+})
---
+++ teclado.js
@@ -0,0 +1,69 @@
+const d =document;
+
+let x= 0;
+let y = 0;
+
+export function moveBall(e,ball,stage){
+const $ball = d.querySelector(ball);
+const $stage =d.querySelector(stage);
+const limitsBall = $ball.getBoundingClientRect();
+const limitsStage = $stage.getBoundingClientRect();
+//console.log(e.keyCode);
+//console.log(e.key);
+//console.log(limitsBall,limitsStage);
+
+switch(e.keyCode){
+    //izquierda
+    case 37:
+        if(limitsBall.left>limitsStage.left){ 
+            e.preventDefault();
+            x--;
+    }
+        break;
+    //arriba
+    case 38:
+        if(limitsBall.top>limitsStage.top){
+            y--;
+            e.preventDefault();
+    } 
+        break;
+    //derecha
+    case 39:
+        if(limitsBall.right<limitsStage.right){
+            e.preventDefault();
+            x++;
+    } 
+        break;
+    //abajo
+    case 40:
+        if(limitsBall.bottom<limitsStage.bottom){
+        e.preventDefault();
+        y++;
+    } 
+        break;    
+    default:
+        break;
+}
+$ball.style.transform =`translate(${x*10}px, ${y*10}px)`;
+}
+
+export function shortcuts(e){
+    /*console.log(e.type);
+    console.log(e.key);
+    console.log(e.keyCode);
+    console.log(e.ctrlKey);
+    console.log(e.altKey);
+    console.log(e.shiftKey);
+    console.log(e);
+    */
+
+    if(e.key === 'a' && e.altKey){
+        alert('Haz lanzado una alerta con el teclado')
+    }
+    if(e.key === 'c' && e.altKey){
+        confirm('Haz lanzado una confirmacion con el teclado')
+    }
+    if(e.key === 'p' && e.altKey){
+        alert('Haz lanzado un aviso con el teclado')
+    }
+}
